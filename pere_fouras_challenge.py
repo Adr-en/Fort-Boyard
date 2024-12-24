14a50810e5f251d5ef5b1a86cb9773e9c0a4bf08
Raphaël <rapha.freser@gmail.com>
2024-12-24 11:56:34 +0100

    Did the pere_fouras_challenge file

---
+++ pere_fouras_challenge.py
@@ -0,0 +1,27 @@
+import json  # as it is a json file, we use the json function to load it into a list of dictionaries
+import random
+
+
+def load_riddles(file):
+    with open(file, "r") as f:
+        return json.load(f)
+
+def pere_fouras_riddles():
+    L = load_riddles("PFRiddles.json")
+    dic = L[random.randint(0, len(L)-1)]
+    print(dic["question"])
+    attempts = 3
+    while attempts > 0:
+        ans = input("Take a guess :").lower()
+        if ans == dic['answer'].lower():
+            print("Correct ! You win a key.")
+            return True
+        else:
+            attempts -= 1
+            if attempts > 0:
+                print(f"Incorrect ! {attempts} attempts remaining")
+            else:
+                print("You failed the challenge ! The correct answer was :", dic['answer'])
+                return True
+
+
